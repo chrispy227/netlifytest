15f6b4338323610a455764df9ec0b5b74bb009bd
chrispy227 <chbppp@gmail.com>
2020-09-09 07:58:55 +0300

    useState with incremental counter and a toggle button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 // Import Components
 import Nav from "./components/Nav";
@@ -8,11 +8,24 @@ function App() {
 	// Write Javascript Here
 	// const name = "developedByChris";
 	// const age = 26;
-	// Array of information
+	const [counter, setCounter] = useState(0);
+	const [toggle, setToggle] = useState(false);
+
+	const incrementor = () => {
+		setCounter((prev) => prev + 1);
+		console.log(counter);
+	};
+	const toggler = () => {
+		setToggle((prev) => !prev);
+	};
+
 	return (
 		<div className="App">
 			<div className="home">
-				<h1>Hello React World</h1>
+				<h1 className={toggle ? "active" : ""}>Hello React World</h1>
+				<h2>Counter {counter}</h2>
+				<button onClick={incrementor}>Click</button>
+				<button onClick={toggler}>Toggle</button>
 				<Nav />
 				<Tweets />
 			</div>
